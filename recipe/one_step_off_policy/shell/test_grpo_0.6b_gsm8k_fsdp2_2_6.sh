90a1979db3fd7386ee9205f9149c323587d4c16c
tju-hwh <1007287849@qq.com>
2025-12-23 17:08:52 +0800

    feat: just merge changes

--- recipe/one_step_off_policy/shell/test_grpo_0.6b_gsm8k_fsdp2_2_6.sh
+++ recipe/one_step_off_policy/shell/test_grpo_0.6b_gsm8k_fsdp2_2_6.sh
@@ -6,26 +6,28 @@ project_name='GRPO'
 exp_name='GRPO-Qwen3-0.6b-gsm8k-fsdp2-one-step-off-2-6'
 
 # Paths
-RAY_DATA_HOME="/home/tank/hwh1/code_reading/meituan_verl"
-MODEL_PATH=${MODEL_PATH:-"${RAY_DATA_HOME}/Qwen3-0.6B"}
+RAY_DATA_HOME="/root/A40_verl"
+MODEL_PATH=${MODEL_PATH:-"/root/model/Qwen2-7B-Instruct"}
 CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
-TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/gsm8k/train.parquet"}
-TEST_FILE=${TEST_FILE:-"${RAY_DATA_HOME}/gsm8k/test.parquet"}
+TRAIN_FILE=${TRAIN_FILE:-"/root/data/gsm8k/train.parquet"}
+TEST_FILE=${TEST_FILE:-"/root/data/gsm8k/test.parquet"}
 
 NNODES=${NNODES:-1}
-NGPUS_PER_NODE=${NGPUS_PER_NODE:-2}
+NGPUS_PER_NODE=${NGPUS_PER_NODE:-4}
 
-n_gpus_rollout=2
+n_gpus_rollout=4
 # n_gpus_training=$((NGPUS_PER_NODE - n_gpus_rollout))
-n_gpus_training=2
+n_gpus_training=4
+
+micro_batch_size=8
 
 python3 -m recipe.one_step_off_policy.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files="${TRAIN_FILE}" \
     data.val_files="${TEST_FILE}" \
-    data.train_batch_size=16 \
-    data.max_prompt_length=128 \
-    data.max_response_length=128 \
+    data.train_batch_size=64 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=4096 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.actor.strategy=fsdp2 \
@@ -34,8 +36,8 @@ python3 -m recipe.one_step_off_policy.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.hybrid_engine=False \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${micro_batch_size} \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -43,15 +45,15 @@ python3 -m recipe.one_step_off_policy.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
-    actor_rollout_ref.rollout.n=1 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
+    actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.layered_summon=True \
-    actor_rollout_ref.rollout.max_num_seqs=256 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.rollout.max_num_seqs=5120 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
@@ -62,8 +64,8 @@ python3 -m recipe.one_step_off_policy.main_ppo \
     trainer.save_freq=0 \
     trainer.test_freq=0 \
     trainer.total_epochs=1 \
-    trainer.total_training_steps=3 \
+    trainer.total_training_steps=20 \
     trainer.nnodes="${NNODES}" \
     trainer.n_gpus_per_node="${n_gpus_training}" \
     rollout.nnodes="${NNODES}" \
-    rollout.n_gpus_per_node="${n_gpus_rollout}" $@ > ./log/log_no.log
+    rollout.n_gpus_per_node="${n_gpus_rollout}" $@ > ./log/log_no_2.log
