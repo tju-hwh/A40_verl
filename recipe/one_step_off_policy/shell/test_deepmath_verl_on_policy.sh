071278fea771af1175dd77f5f0e8fad0d299073b
tju-hwh <1007287849@qq.com>
2026-01-08 20:08:23 +0800

    origin verl, the truth throughput is 1082

---
+++ recipe/one_step_off_policy/shell/test_deepmath_verl_on_policy.sh
@@ -0,0 +1,68 @@
+export mylog="1"
+
+set -x
+
+project_name='GRPO'
+exp_name='GRPO-Qwen3-0.6b-deepmath-fsdp2-one-step-off-2-6'
+
+# Paths
+RAY_DATA_HOME="/root/A40_verl"
+MODEL_PATH=${MODEL_PATH:-"/root/model/Qwen2-7B-Instruct"}
+CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
+TRAIN_FILE=${TRAIN_FILE:-"/root/data/deepmath/train.parquet"}
+TEST_FILE=${TEST_FILE:-"/root/data/deepmath/test.parquet"}
+
+NNODES=${NNODES:-1}
+NGPUS_PER_NODE=${NGPUS_PER_NODE:-4}
+
+n_gpus_rollout=4
+# n_gpus_training=$((NGPUS_PER_NODE - n_gpus_rollout))
+n_gpus_training=4
+
+micro_batch_size=4
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files="${TRAIN_FILE}" \
+    data.val_files="${TEST_FILE}" \
+    data.train_batch_size=32 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=3072 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.actor.strategy=fsdp2 \
+    critic.strategy=fsdp2 \
+    actor_rollout_ref.model.path="${MODEL_PATH}" \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${micro_batch_size} \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
+    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.rollout.load_format=safetensors \
+    actor_rollout_ref.rollout.layered_summon=True \
+    actor_rollout_ref.rollout.max_num_seqs=128 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.use_kl_in_reward=False \
+    trainer.critic_warmup=0 \
+    trainer.val_before_train=False \
+    trainer.logger=['console','tensorboard'] \
+    trainer.project_name="${project_name}" \
+    trainer.experiment_name="${exp_name}" \
+    trainer.save_freq=0 \
+    trainer.test_freq=0 \
+    trainer.total_epochs=1 \
+    trainer.total_training_steps=10 \
+    trainer.nnodes="${NNODES}" \
+    trainer.n_gpus_per_node="${n_gpus_training}"  $@ > ./log/origin_on_policy_verl_deepmath_02.log
