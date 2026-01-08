2ede2da52791bd30e1cd9818f08d922cc00316bc
tju-hwh <1007287849@qq.com>
2026-01-08 16:07:47 +0800

    add deepmath, successful

--- recipe/one_step_off_policy/shell/test_deepmath_verl_on_policy.sh
+++ recipe/one_step_off_policy/shell/test_deepmath_verl_on_policy.sh
@@ -19,6 +19,8 @@ n_gpus_rollout=4
 # n_gpus_training=$((NGPUS_PER_NODE - n_gpus_rollout))
 n_gpus_training=4
 
+micro_batch_size=4
+
 python3 -m recipe.one_step_off_policy.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files="${TRAIN_FILE}" \
@@ -46,11 +48,11 @@ python3 -m recipe.one_step_off_policy.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
     actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.layered_summon=True \
-    actor_rollout_ref.rollout.max_num_seqs=256 \
+    actor_rollout_ref.rollout.max_num_seqs=128 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
@@ -62,8 +64,8 @@ python3 -m recipe.one_step_off_policy.main_ppo \
     trainer.save_freq=0 \
     trainer.test_freq=0 \
     trainer.total_epochs=1 \
-    trainer.total_training_steps=3 \
+    trainer.total_training_steps=10 \
     trainer.nnodes="${NNODES}" \
     trainer.n_gpus_per_node="${n_gpus_training}" \
     rollout.nnodes="${NNODES}" \
-    rollout.n_gpus_per_node="${n_gpus_rollout}" $@ > ./log/log_no.log
+    rollout.n_gpus_per_node="${n_gpus_rollout}" $@ > ./log/log_verl_deepmath_01.log
