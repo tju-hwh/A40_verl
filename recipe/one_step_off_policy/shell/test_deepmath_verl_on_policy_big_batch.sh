cb4dda1c38ee3df77d6e12a6ef59cf2939710311
tju-hwh <1007287849@qq.com>
2026-01-14 08:24:20 +0000

    success exec

--- recipe/one_step_off_policy/shell/test_deepmath_verl_on_policy_big_batch.sh
+++ recipe/one_step_off_policy/shell/test_deepmath_verl_on_policy_big_batch.sh
@@ -6,33 +6,29 @@ project_name='GRPO'
 exp_name='GRPO-Qwen3-0.6b-deepmath-fsdp2-one-step-off-2-6'
 
 # Paths
-RAY_DATA_HOME="/root/A40_verl"
-MODEL_PATH=${MODEL_PATH:-"/root/model/Qwen2-7B-Instruct"}
-CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
-TRAIN_FILE=${TRAIN_FILE:-"/root/data/deepmath/train.parquet"}
-TEST_FILE=${TEST_FILE:-"/root/data/deepmath/test.parquet"}
+
 
 NNODES=${NNODES:-1}
-NGPUS_PER_NODE=${NGPUS_PER_NODE:-4}
+NGPUS_PER_NODE=${NGPUS_PER_NODE:-1}
 
-n_gpus_rollout=4
+n_gpus_rollout=1
 # n_gpus_training=$((NGPUS_PER_NODE - n_gpus_rollout))
-n_gpus_training=4
+n_gpus_training=1
 
-micro_batch_size=8
+micro_batch_size=32
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
-    data.train_files="${TRAIN_FILE}" \
-    data.val_files="${TEST_FILE}" \
+    data.train_files=/openbayes/home/data/gsm8k/train.parquet \
+    data.val_files=/openbayes/home/data/gsm8k/test.parquet \
     data.train_batch_size=128 \
     data.max_prompt_length=1024 \
-    data.max_response_length=3072 \
+    data.max_response_length=1024 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.actor.strategy=fsdp2 \
     critic.strategy=fsdp2 \
-    actor_rollout_ref.model.path="${MODEL_PATH}" \
+    actor_rollout_ref.model.path="/openbayes/home/models/Qwen2.5-0.5B" \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
@@ -45,9 +41,9 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${micro_batch_size} \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.load_format=safetensors \
     actor_rollout_ref.rollout.layered_summon=True \
@@ -58,11 +54,11 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.val_before_train=False \
     trainer.logger=['console','tensorboard'] \
-    trainer.project_name="${project_name}" \
-    trainer.experiment_name="${exp_name}" \
+    trainer.project_name="test01" \
+    trainer.experiment_name="test01" \
     trainer.save_freq=0 \
     trainer.test_freq=0 \
     trainer.total_epochs=1 \
     trainer.total_training_steps=10 \
     trainer.nnodes="${NNODES}" \
-    trainer.n_gpus_per_node="${n_gpus_training}"  $@ > ./log/origin_on_policy_verl_deepmath_02.log
+    trainer.n_gpus_per_node="${n_gpus_training}" 2>&1 | tee ../test123.log
